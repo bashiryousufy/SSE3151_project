50b7d999e6b9c368edbce357ea8d819b44f7bb2a
bashiryousufy <bashiryousufy@gmail.com>
2022-02-18 17:39:47 +0800

    bug fixes

--- lib/Screens/folders_screen.dart
+++ lib/Screens/folders_screen.dart
@@ -256,8 +256,7 @@ class _FolderScreenState extends State<FolderScreen> {
                                 return InkWell(
                                   onTap: () {
                                     Navigator.push(context,
-                                        new MaterialPageRoute(
-                                            builder: (builder) {
+                                        MaterialPageRoute(builder: (builder) {
                                       return InnerFolder(
                                           filespath: _folders[index].path);
                                     }));
